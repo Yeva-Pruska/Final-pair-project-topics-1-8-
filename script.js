826187a893a534df0635ec3b50605ca89b92e42b
Yeva Pruska <prusskaya.ev.a@gmail.com>
2026-06-15 17:02:59 +0300

    Added stage 5, fixed errors.

--- script.js
+++ script.js
@@ -6,16 +6,24 @@ let costs = [5000, 3000, 8000, 1000]
 let bugs = [45, 10, 28, 5]
 
 //  Етап 1 QA
-let i = 0
-while (i < bugs.length) {
+//*
+for(let i = bugs.length - 1; i >= 0; i--){
 	if (bugs[i] > 30) {
 		features.splice(i, 1);
 		costs.splice(i, 1);
 		bugs.splice(i, 1);
-	} else {
-		i++;
-	}
 }
+}
+// let i = 0
+// while (i < bugs.length) {
+// 	if (bugs[i] > 30) {
+// 		features.splice(i, 1);
+// 		costs.splice(i, 1);
+// 		bugs.splice(i, 1);
+// 	} else {
+// 		i++;
+// 	}
+// }
 
 //Етап 2 Збір даних та жорстка валідація
 let studioName;
@@ -54,7 +62,7 @@ console.log(months)
 
 
 
-
+//Етап 3
 let gameEditionType;
 
 do{
@@ -81,5 +89,52 @@ switch(gameEditionType){
         console.log(gameEditionType)
 
 }
+
+//Етап 4 Кранч та вирізання фіч 
+let featureFound = false;
+while (featureFound === false) {
+    let featuresList = features.join(", ");
+    let answer = prompt("Видавництво вимагає вирізати фічу" + featuresList);
+    if (answer === null || answer.trim() === "") {
+        alert("Ви нічого не вводили. Сробуйте ще раз!");
+        continue;
+    }
+let lowerCaseFeatures = [];
+for (let i = 0; i < features.length; i++) {
+    lowerCaseFeatures.push(features[i].toLowerCase());
+}
+let answerLower = answer.toLowerCase();
+let foundIndex = lowerCaseFeatures.indexOf(answerLower);
+if (foundIndex !== -1) {
+    features.splice(foundIndex, 1);
+    costs.splice(foundIndex, 1);
+    bugs.splice(foundIndex, 1);
+
+    alert("Фічу успішно видалено!");
+    featureFound = true;
+} else { alert("Такої фічі не знайдено! Спробуйте ще раз")}; //*
+}
+
+
+
+
+//Етап 5
+let totalCost = 0;
+for(let i = 0; i < costs.length; i++){
+    totalCost = totalCost + costs[i]
+} console.log(totalCost)
+
+if(months > 12){
+    totalCost = totalCost * 1.15;
+} else if (months < 6){
+    totalCost = totalCost * 0.9
+} 
+
+if(budget >= totalCost){
+    let x = "";
+   for(let i = 0; i < features.length; i++){
+    x = x + `• Фіча: ${features[i]} | Ціна: ${costs[i]}$ | Багів: ${bugs[i]} \n`;
+   } alert(`🚀 Реліз у Steam успішний!\n\nСтудія "${studioName}" випустила гру "${gameName}"!\n\n${x}\n💰 Фінальна вартість розробки: ${totalCost}$`);
+} else { alert(`Банкрутство! Студії "${studioName}" Не вистачило ${totalCost - budget}$ для завершення гри "${gameName}"`)}
 
 })
